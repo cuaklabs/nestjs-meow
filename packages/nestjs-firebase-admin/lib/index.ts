1124b158291550c84dfc0814e0621a82969b0578
Adrian <adrian.martinez@8belts.com>
2021-12-16 12:45:38 +0100

    Added typeguards

--- packages/nestjs-firebase-admin/lib/index.ts
+++ packages/nestjs-firebase-admin/lib/index.ts
@@ -1,5 +1,17 @@
-import { AppAsyncOptions } from './modules/AppAsyncOptions';
+import { AppAsyncOptions } from './models/AppAsyncOptions';
+import { AppClassAsyncOptions } from './models/AppClassAsyncOptions';
+import { AppExistingAsyncOptions } from './models/AppExistingAsyncOptions';
+import { AppFactoryAsyncOptions } from './models/AppFactoryAsyncOptions';
+import { AppOptionsFactory } from './models/AppOptionsFactory';
 import { FirebaseAdminModule } from './modules/FirebaseAdminModule';
 import { FirebaseProviderType } from './modules/FirebaseProviderType';
 
-export { FirebaseAdminModule, FirebaseProviderType, AppAsyncOptions };
+export {
+  FirebaseAdminModule,
+  FirebaseProviderType,
+  AppAsyncOptions,
+  AppFactoryAsyncOptions,
+  AppClassAsyncOptions,
+  AppExistingAsyncOptions,
+  AppOptionsFactory,
+};
