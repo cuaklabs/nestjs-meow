40677efd6e7b0b37f2d2ebe2faa91e6da3d44dd0
Adrian <adrian.martinez@8belts.com>
2021-12-16 12:45:21 +0100

    moved providers

--- packages/nestjs-firebase-admin/lib/modules/FirebaseAdminModule.ts
+++ packages/nestjs-firebase-admin/lib/modules/FirebaseAdminModule.ts
@@ -1,75 +1,30 @@
-/* eslint-disable import/no-unresolved */
 import { DynamicModule, Module } from '@nestjs/common';
 import { AppOptions } from 'firebase-admin';
-import { initializeApp } from 'firebase-admin/app';
-import { AppCheck, getAppCheck } from 'firebase-admin/app-check';
-import { Auth, getAuth } from 'firebase-admin/auth';
-import { Firestore, getFirestore } from 'firebase-admin/firestore';
-import { getInstallations, Installations } from 'firebase-admin/installations';
-import { getMachineLearning, MachineLearning } from 'firebase-admin/machine-learning';
-import { getMessaging, Messaging } from 'firebase-admin/messaging';
-import { getProjectManagement, ProjectManagement } from 'firebase-admin/project-management';
-import { getRemoteConfig, RemoteConfig } from 'firebase-admin/remote-config';
-import { getSecurityRules, SecurityRules } from 'firebase-admin/security-rules';
-import { getStorage, Storage } from 'firebase-admin/storage';
 
-import { AppAsyncOptions } from './AppAsyncOptions';
-import { FirebaseProviderInstance, FirebaseProviderType } from './FirebaseProviderType';
+import { AppAsyncOptions } from '../models/AppAsyncOptions';
+import { FirebaseAdminCoreModule } from './FirebaseAdminCoreModule';
+import { FirebaseProviderType } from './FirebaseProviderType';
 
 @Module({})
 export class FirebaseAdminModule {
-  private static readonly providers: Map<FirebaseProviderType, FirebaseProviderInstance> = new Map();
-  private static readonly initFunctions: Map<FirebaseProviderType, () => FirebaseProviderInstance> = new Map();
-
-  public static forRoot(options: AppOptions, initProviders?: FirebaseProviderType[]): DynamicModule {
-    initializeApp(options);
-
-    this.initFunctions.set(Auth, getAuth);
-    this.initFunctions.set(Firestore, getFirestore);
-    this.initFunctions.set(Messaging, getMessaging);
-    this.initFunctions.set(AppCheck, getAppCheck);
-    this.initFunctions.set(Installations, getInstallations);
-    this.initFunctions.set(MachineLearning, getMachineLearning);
-    this.initFunctions.set(ProjectManagement, getProjectManagement);
-    this.initFunctions.set(RemoteConfig, getRemoteConfig);
-    this.initFunctions.set(SecurityRules, getSecurityRules);
-    this.initFunctions.set(Storage, getStorage);
-
-    return initProviders === undefined
-      ? {
-          module: FirebaseAdminModule,
-        }
-      : this.getFeature(initProviders);
+  public static forRoot(appOptions: AppOptions): DynamicModule {
+    return {
+      imports: [FirebaseAdminCoreModule.forRoot(appOptions)],
+      module: FirebaseAdminModule,
+    };
   }
 
   public static getFeature(providers: FirebaseProviderType[]): DynamicModule {
     return {
       exports: providers,
+      imports: [FirebaseAdminCoreModule],
       module: FirebaseAdminModule,
-      providers: providers.map((element: FirebaseProviderType) => {
-        let provider: FirebaseProviderInstance | undefined = this.providers.get(element);
-
-        if (provider === undefined) {
-          const providerBuilder: () => FirebaseProviderInstance = this.initFunctions.get(
-            element,
-          ) as () => FirebaseProviderInstance;
-          provider = providerBuilder();
-          this.providers.set(element, provider);
-        }
-
-        return {
-          provide: element,
-          useValue: provider,
-        };
-      }),
     };
   }
 
-  public static forRootAsync(options: AppAsyncOptions): DynamicModule {
-    if (options.useClass === undefined && options.useExisting === undefined && options.useFactory === undefined) {
-      throw new Error('Configuration error');
-    }
+  public static forRootAsync(appAsyncOptions: AppAsyncOptions): DynamicModule {
     return {
+      imports: [FirebaseAdminCoreModule.forRootAsync(appAsyncOptions)],
       module: FirebaseAdminModule,
     };
   }
---
+++ packages/nestjs-firebase-admin/lib/modules/firebaseAdminCoreModuleProviders.ts
@@ -0,0 +1,88 @@
+/* eslint-disable import/no-unresolved */
+import { Provider } from '@nestjs/common';
+import { AppCheck, getAppCheck } from 'firebase-admin/app-check';
+import { Auth, getAuth } from 'firebase-admin/auth';
+import { Firestore, getFirestore } from 'firebase-admin/firestore';
+import { getInstallations, Installations } from 'firebase-admin/installations';
+import { getMachineLearning, MachineLearning } from 'firebase-admin/machine-learning';
+import { getMessaging, Messaging } from 'firebase-admin/messaging';
+import { getProjectManagement, ProjectManagement } from 'firebase-admin/project-management';
+import { getRemoteConfig, RemoteConfig } from 'firebase-admin/remote-config';
+import { getSecurityRules, SecurityRules } from 'firebase-admin/security-rules';
+import { getStorage, Storage } from 'firebase-admin/storage';
+
+import { AppInitializer } from './AppInitializer';
+import { FirebaseProviderInstance } from './FirebaseProviderType';
+
+export const providers: Provider<FirebaseProviderInstance>[] = [
+  {
+    provide: Auth,
+    useFactory: (appInitializer: AppInitializer) => {
+      appInitializer.initialize();
+      return getAuth();
+    },
+  },
+  {
+    provide: Firestore,
+    useFactory: (appInitializer: AppInitializer) => {
+      appInitializer.initialize();
+      return getFirestore();
+    },
+  },
+  {
+    provide: Messaging,
+    useFactory: (appInitializer: AppInitializer) => {
+      appInitializer.initialize();
+      return getMessaging();
+    },
+  },
+  {
+    provide: AppCheck,
+    useFactory: (appInitializer: AppInitializer) => {
+      appInitializer.initialize();
+      return getAppCheck();
+    },
+  },
+  {
+    provide: Installations,
+    useFactory: (appInitializer: AppInitializer) => {
+      appInitializer.initialize();
+      return getInstallations();
+    },
+  },
+  {
+    provide: MachineLearning,
+    useFactory: (appInitializer: AppInitializer) => {
+      appInitializer.initialize();
+      return getMachineLearning();
+    },
+  },
+  {
+    provide: ProjectManagement,
+    useFactory: (appInitializer: AppInitializer) => {
+      appInitializer.initialize();
+      return getProjectManagement();
+    },
+  },
+  {
+    provide: RemoteConfig,
+    useFactory: (appInitializer: AppInitializer) => {
+      appInitializer.initialize();
+      return getRemoteConfig();
+    },
+  },
+  {
+    provide: SecurityRules,
+    useFactory: (appInitializer: AppInitializer) => {
+      appInitializer.initialize();
+      return getSecurityRules();
+    },
+  },
+  {
+    provide: Storage,
+    useFactory: (appInitializer: AppInitializer) => {
+      appInitializer.initialize();
+      return getStorage();
+    },
+  },
+];
