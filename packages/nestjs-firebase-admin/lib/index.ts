198a89049e6d2653121ba835bf3b2ce7723b76cf
notaphplover <roberto.pintos.lopez@gmail.com>
2021-12-23 17:56:31 +0100

    feat(nestjs-firebase-admin): update index with missing types

--- packages/nestjs-firebase-admin/lib/index.ts
+++ packages/nestjs-firebase-admin/lib/index.ts
@@ -1,7 +1,9 @@
 import { FirebaseType } from './models/FirebaseType';
+import { NameAppOptionsPair } from './models/NameAppOptionsPair';
 import { NestFirebaseAdminAppAsyncOptions } from './models/NestFirebaseAdminAppAsyncOptions';
 import { NestFirebaseAdminAppClassAsyncOptions } from './models/NestFirebaseAdminAppClassAsyncOptions';
 import { NestFirebaseAdminAppFactoryAsyncOptions } from './models/NestFirebaseAdminAppFactoryAsyncOptions';
+import { NestFirebaseAdminAppOptions } from './models/NestFirebaseAdminAppOptions';
 import { NestFirebaseAdminAppOptionsFactory } from './models/NestFirebaseAdminAppOptionsFactory';
 import { FirebaseAdminModule } from './modules/FirebaseAdminModule';
 import { InjectFirebaseProvider } from './modules/InjectFirebaseProvider';
@@ -9,9 +11,11 @@ import { InjectFirebaseProvider } from './modules/InjectFirebaseProvider';
 export {
   FirebaseAdminModule,
   FirebaseType,
+  NameAppOptionsPair,
   NestFirebaseAdminAppAsyncOptions,
   NestFirebaseAdminAppFactoryAsyncOptions,
   NestFirebaseAdminAppClassAsyncOptions,
+  NestFirebaseAdminAppOptions,
   NestFirebaseAdminAppOptionsFactory,
   InjectFirebaseProvider,
 };
